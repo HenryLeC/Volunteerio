225741d5f6abc5d956a9da7a8d1d580cae187965
Henry LeCompte <lecompteh18@gmail.com>
2020-05-09 22:33:18 -0400

    Added Color Pallete for Menu Button and Header color

---
+++ Volunteerio/ImageResourceExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Volunteerio
+{
+    class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            ImageSource imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
