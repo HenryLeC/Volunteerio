8c2b86b729d255ba0138a036e2535b309e043c67
Henry LeCompte <henry@volunteerio.us>
2020-08-28 17:19:56 -0400

    Added Password Reseting

---
+++ Volunteerio/Views/ResetPassword.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Volunteerio.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResetPassword : ContentPage
+    {
+        public ResetPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void Request_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string response = APIRequest.Request("resetPassword", false, new Dictionary<string, string>
+                {
+                    {"uname", Uname.Text }
+                });
+
+                Dictionary<string, string> ResponseDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+
+                DisplayAlert(ResponseDict["title"], ResponseDict["msg"], "Ok");
+            }
+            catch
+            {
+                DisplayAlert("Server Error", "Server Error, Please Try Again Later", "Ok");
+            }
+        }
+    }
+}
