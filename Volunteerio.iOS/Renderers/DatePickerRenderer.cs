1667c40b99b2d3210b706afe1fbd01478b3e14b6
Henry LeCompte <henry@volunteerio.us>
2020-10-02 09:48:48 -0400

    Removed System.Drawing (Didnt Work)

---
+++ Volunteerio.iOS/Renderers/DatePickerRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UIKit;
+using Xamarin.Forms;
+using Volunteerio.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(DatePickerRenderer))]
+namespace Volunteerio.iOS.Renderers
+{
+	class DatePickerRenderer : Xamarin.Forms.Platform.iOS.DatePickerRenderer
+	{
+		protected override void OnElementChanged(Xamarin.Forms.Platform.iOS.ElementChangedEventArgs<DatePicker> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				UITextField entry = Control;
+				UIDatePicker picker = (UIDatePicker)entry.InputView;
+				picker.PreferredDatePickerStyle = UIDatePickerStyle.Wheels;
+			}
+		}
+	}
+}
---
+++ Volunteerio.iOS/Renderers/TimePickerRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UIKit;
+using Xamarin.Forms;
+using Volunteerio.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(TimePicker), typeof(TimePickerRenderer))]
+namespace Volunteerio.iOS.Renderers
+{
+    class TimePickerRenderer : Xamarin.Forms.Platform.iOS.TimePickerRenderer
+    {
+		protected override void OnElementChanged(Xamarin.Forms.Platform.iOS.ElementChangedEventArgs<TimePicker> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				UITextField entry = Control;
+				UIDatePicker picker = (UIDatePicker)entry.InputView;
+				picker.PreferredDatePickerStyle = UIDatePickerStyle.Wheels;
+			}
+		}
+	}
+}
